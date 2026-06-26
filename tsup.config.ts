27bafd1c649f3574199cbb15a6a75ae4af20091c
yagizdo <dokumaciyagiz@gmail.com>
2026-06-26 11:50:11 +0300

    chore: initial project scaffold
    package.json, tsconfig, tsup, vitest, .gitignore, LICENSE

---
+++ tsup.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: ['src/cli.ts'],
+  format: ['esm'],
+  target: 'node22',
+  outDir: 'dist',
+  clean: true,
+  bundle: true,
+  loader: { '.json': 'json' },
+  banner: {
+    js: '#!/usr/bin/env node',
+  },
+})
